1ebf747123792b7cd60ddd45640443e8c45560c2
Sushil Thakur <60125019+rsusheel@users.noreply.github.com>
2020-06-16 23:17:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -272,7 +272,7 @@ observerEleven.observe(obsItemEleven);
 var observerTwelve = new IntersectionObserver(
     (entry) => {
         if(entry[0].intersectionRatio > 0.7){ 
-            document.getElementsByClassName('two-block')[0].innerHTML = '<span class="thisis">THIS IS MY</span><span class="portfolio">PORTFOLIO</span><span class="resume-pg">Resume</span>';
+            document.getElementsByClassName('two-block')[0].innerHTML = '<span class="thisis">THIS IS MY</span><span class="portfolio">PORTFOLIO</span><a class="resume-pg" href="resume.html">Resume</a>';
         }else if(bbb.innerHTML === '<span class="thisis">THIS IS MY</span><span class="portfolio">PORTFOLIO</span>' && entry[0].intersectionRatio < 0.6){
             document.getElementsByClassName('two-block')[0].innerHTML = "AND";
         }
